9f0d910429c0c086fbf91cd938cf95461291fc1c
DualDarkElu <35860367+DualDarkElu@users.noreply.github.com>
2023-10-04 14:28:57 +0900

    p12

---
+++ P12/P12/My_op.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int my_op(int x, int y, int op);
+void decimal_convert(int x, int y);
+
+#define PLUS 1
+#define MINUS 2
+#define MULTIPLY 3
+#define DIVIDE 4
+#define REMAINDER 5
+#define DECIMAL_CONVERT 6
